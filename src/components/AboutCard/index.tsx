97c2b41f8285456997037524f6461c9b3bd01faf
victoria <vikabirukova22@gmail.com>
2025-01-24 17:53:13 +0200

    feat: about section

---
+++ src/components/AboutCard/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index.css';
+
+type Props = {
+  title: string;
+  description: string;
+  src: string;
+};
+
+const AboutCard = ({ title, description, src }: Props) => {
+  return (
+    <section className='about-card'>
+      <div className='about-card__text-container'>
+        <h2 className='about-card__title'>{title}</h2>
+        <p className='about-card__desc'>{description}</p>
+      </div>
+      <div className='about-card__image-wrapper'>
+        <img className='about-card__image' src={`images/${src}`} alt='image' />
+      </div>
+    </section>
+  );
+};
+
+export default AboutCard;
--- src/compositions/AboutSection/index.tsx
+++ src/compositions/AboutSection/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import './index.css';
+import Container from '../Container';
+import AboutCard from '../../components/AboutCard';
+import Button from '../../components/Button';
 
 type Props = {};
 
 const AboutSection = (props: Props) => {
-  return <div>AboutSection</div>;
+  return (
+    <div className='about'>
+      <Container>
+        <section className='about-first'>
+          <h2 className='about-first__title'>
+            Ми - фонд підтримки постраждалих від вйни
+          </h2>
+          <p className='about-first__subtitle'>
+            Наша мета – психологічна та гуманітарна допомога українцям
+          </p>
+          <p className='about-first__desc'>
+            Кожен, хто звернеться, отримає допомогу, яка у наших силах
+          </p>
+        </section>
+        <AboutCard
+          title='Реабілітаційний центр'
+          description='Ми допомагаємо людям, які постраждали знайти правильне лікування. У нашому фонді є чотири напрями: фізичної та реабілітаційної медицини, терапевтичне, неврологічне та хірургічне. Ми маємо заклади-партнери, де швидко та якісно можуть надати допомогу.'
+          src='about-1.png'
+        />
+        <AboutCard
+          title='Робота з дітьми'
+          description='Діти – найвразливіша частина суспільства, і їм особливо потрібна підтримка дорослих. Тому наш фонд збирає речі для дітей, проводить літні й зимові табори, майстер-класи та за потреби – дитячих прсихотерапевтів.'
+          src='about-2.png'
+        />
+        <Button text='Дізнатися більше' />
+      </Container>
+    </div>
+  );
 };
 
 export default AboutSection;
