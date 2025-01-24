51745167f82cda2be4872af67dc466e348e5a85d
victoria <vikabirukova22@gmail.com>
2025-01-24 17:53:00 +0200

    feat: create hero section

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,10 +3,15 @@ import './index.css';
 
 type IButtonProps = {
   text: string;
+  color?: string;
 };
 
-const Button = ({ text }: IButtonProps) => {
-  return <button className='button'>{text}</button>;
+const Button = ({ text, color }: IButtonProps) => {
+  return (
+    <button color={color} className='button'>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
--- src/compositions/HeroSection/index.tsx
+++ src/compositions/HeroSection/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import './index.css';
+import Container from '../Container';
 
 type Props = {};
 
 const HeroSection = (props: Props) => {
-  return <div>HeroSection</div>;
+  return (
+    <section className='hero__container'>
+      <Container>
+        <div className='hero__text-container'>
+          <h1 className='hero__title'>Фонд підтримки постраждалих від війни</h1>
+          <p className='hero__desc'>
+            Ми хочемо допомогти кожному, хто постраждав під час війни
+          </p>
+          <button className='hero__button'>Підтримати фонд</button>
+        </div>
+      </Container>
+    </section>
+  );
 };
 
 export default HeroSection;
