535cde07bf37a390b68114b008aff24caa6ac516
victoria <vikabirukova22@gmail.com>
2025-01-24 15:58:55 +0200

    feat: create header component

---
+++ src/compositions/Container/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+type IContainerProps = {
+  children: React.ReactNode;
+};
+
+const Container = ({ children }: IContainerProps) => {
+  return <div className='container'>{children}</div>;
+};
+
+export default Container;
--- src/compositions/Header/index.tsx
+++ src/compositions/Header/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Container from '../Container';
+import './index.css';
+import Menu from '../../components/Menu';
 
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <header className='header'>
+      <Container>
+        <div className='header__inner-container'>
+          <div className='header__logo-container'>
+            <img src='images/logo.png' alt='logo' width='65' />
+          </div>
+          <Menu />
+        </div>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
